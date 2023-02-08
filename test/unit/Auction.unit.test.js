034ac4e71942c00c3f7474c5085693b1e4ba9a01
ggulaman <gulamman@protonmail.com>
2023-02-08 02:36:12 +0000

    updating smart contract test cases

--- test/unit/Auction.unit.test.js
+++ test/unit/Auction.unit.test.js
@@ -7,20 +7,18 @@ const BN = require('bn.js');
 // Enable and inject BN dependency
 chai.use(require('chai-bn')(BN));
 
-const auctionMinBid = new ethers.BigNumber.from('100000000000000000');
-const auctionMinPrice = new ethers.BigNumber.from('1000000000000000000');
 const auctionDuration = 60 * 60; // 1 hour in seconds
 const currentTime = Date.now() / 1000 | 0; // in seconds
 const ERC20Name = "AuctionCoin";
 const ERC20Symbol = "AC"; 
-const ERC20Supply = 21000000;
+const ERC20Supply = 10;
 
 describe('Auction Unit Test', function() { 
     before(async () => {
-        [owner, signer0, signer1, signer2] = await ethers.getSigners()
+        [owner, signer0, signer1, signer2, signer3, signer4, signer5] = await ethers.getSigners()
         auctionDef = await ethers.getContractFactory('Auction')
         auction = await auctionDef.connect(signer0).deploy(
-            owner.getAddress(), auctionMinBid, auctionMinPrice, auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply
+            owner.getAddress(), auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply
         )
         await auction.deployed()
         auctionSCAddress = auction.address;
@@ -42,93 +40,105 @@ describe('Auction Unit Test', function() {
         expect(ownerAddress).to.not.be.equal(owner.getAddress())
     })
 
-    it('returns the Address of the Auction Winner for now', async () => {
-        const winnerAddress = await auction.connect(signer0).getAuctionWinner();
-        expect(winnerAddress).to.not.be.equal("0x")
-    })
-
     it('returns the deadline of the auction', async () => {
         const returnedAuctionDeadline = await auction.connect(signer0).getAuctionDeadline();
         expect(returnedAuctionDeadline).to.be.greaterThan(currentTime)
     })
 
-    it('returns the min bid increment amount', async () => {
-        const returnedMinIncrement = await auction.connect(signer0).getMinBidIncrement();
-        expect(returnedMinIncrement).to.be.equal(auctionMinBid)
-    })
-
-    it('returns the starting price', async () => {
-        const returnedMinPrice = await auction.connect(signer0).getMinPrice();
-        expect(auctionMinPrice).to.be.equal(returnedMinPrice)
-    })
-
-    it('returns the current higer bid', async () => {
-        const higherBid = await auction.connect(signer0).getCurrentPrice();
-        expect(higherBid).to.be.equal(new ethers.BigNumber.from('900000000000000000'))
-    })
-
     it('prevents owner from staking', async () => {
-        await expect(auction.connect(owner).auctionBid()).to.be.revertedWith("Owner cannot deposit")
+        const price = 1;
+        const amount = 1;
+        await expect(auction.connect(owner).auctionBid(price, amount)).to.be.revertedWith("Owner cannot deposit")
     });
 
     it('allows signer0 to auctionBid', async () => {
-        scBalance0 = await auction.provider.getBalance(auctionSCAddress);
-        expect(scBalance0).to.be.equal(new ethers.BigNumber.from('0'));
-        await auction.connect(signer0).auctionBid({ value: '10000000000000000000' });
+        const price = 100;
+        const amount = 2;
+
+        await auction.connect(signer0).auctionBid(price, amount);
+
+        const [totalPriceAft, AmountAft] = await auction.connect(signer0).getAmountAndPriceClaimableByBidder(await signer0.getAddress());
         scBalance1 = await auction.provider.getBalance(auctionSCAddress);
-        expect(scBalance1).to.be.greaterThan(scBalance0);
+        expect(totalPriceAft).to.be.equal(200);
+        expect(AmountAft).to.be.equal(2);
     });
 
-    it('allows signer1 to auctionBid, and funds are send back to signer0', async () => {
-        const signer0Balance0 = await signer0.getBalance();
-        const signer1Balance0 = await signer1.getBalance();
+    it('calculates the correct price for the Min. Valid Price', async () => {
+        await auction.connect(signer2).auctionBid(400, 4);
+        await auction.connect(signer4).auctionBid(600, 1);
+        await auction.connect(signer5).auctionBid(800, 3);
+        await auction.connect(signer3).auctionBid(500, 1);
+        await auction.connect(signer1).auctionBid(200, 3);
+        await auction.connect(signer5).auctionBid(800, 5);
+
+        const currentMinPrice = await auction.connect(signer0).getCurrentMinPrice();
+        expect(currentMinPrice).to.be.equal(500);
+
+        const [totalPriceAft, AmountAft] = await auction.connect(signer3).getAmountAndPriceClaimableByBidder(await signer3.getAddress());
+        expect(totalPriceAft).to.be.equal(500);
+        expect(AmountAft).to.be.equal(1);
+    });
+
+
+    it('prevents signer3 from claiming after another bidder matches its amount', async () => {
+        await auction.connect(signer5).auctionBid(500, 1);
+        const currentMinPrice = await auction.connect(signer0).getCurrentMinPrice();
+        expect(currentMinPrice).to.be.equal(500);
+
+        const [totalPriceAft, AmountAft] = await auction.connect(signer3).getAmountAndPriceClaimableByBidder(await signer3.getAddress());
+        expect(totalPriceAft).to.be.equal(0);
+        expect(AmountAft).to.be.equal(0);
+    });
+
+    it('raises and error when bidder place a bid for an amount grater than the supply', async () => {
+        await expect(auction.connect(signer2).auctionBid(400, ERC20Supply + 1)).to.be.revertedWith("Amount must be lower than supply");
+    });
+
+    // it('allows signer1 to auctionBid, and funds are send back to signer0', async () => {
+    //     const signer0Balance0 = await signer0.getBalance();
+    //     const signer1Balance0 = await signer1.getBalance();
         
-        await auction.connect(signer1).auctionBid({ value: '20000000000000000000' });
+    //     await auction.connect(signer1).auctionBid({ value: '20000000000000000000' });
 
-        const signer0Balance1 = await signer0.getBalance();
-        const signer1Balance1 = await signer1.getBalance();
+    //     const signer0Balance1 = await signer0.getBalance();
+    //     const signer1Balance1 = await signer1.getBalance();
 
-        expect(signer0Balance0).to.be.lessThan(signer0Balance1);
-        expect(signer1Balance0).to.be.greaterThan(signer1Balance1);
-    });
+    //     expect(signer0Balance0).to.be.lessThan(signer0Balance1);
+    //     expect(signer1Balance0).to.be.greaterThan(signer1Balance1);
+    // });
 
-    it('prevents users from staking a low amount', async () => {
-        await expect(auction.connect(signer2).auctionBid({ value: '20000000000000000001' })).to.be.revertedWith("Low stake")
-    });
+    // it('prevents users from staking a low amount', async () => {
+    //     await expect(auction.connect(signer2).auctionBid({ value: '20000000000000000001' })).to.be.revertedWith("Low stake")
+    // });
 
     it('prevents from claiming until the auction is finished', async () => {
         await expect(auction.connect(signer1).claimBid()).to.be.revertedWith("auction is running")
     });
 
-    it('prevents a user of of claiming the auction until it finishes', async () => {
-        await expect(auction.connect(owner).claimBid()).to.be.revertedWith("auction is running")
-    });
-
-    it('only allows signer1 to claim, after the auction is inactive', async () => {
+    it('prevents signer3 from claiming, as does not have a winning bid', async () => {
         auctionIsActive0 = await auction.connect(signer1).getIfAuctionIsActive();
         expect(auctionIsActive0).to.be.true;
-        // mine 1000 blocks with an interval of 1 minute
-        await hre.network.provider.send("hardhat_mine", ["0x3e8", "0x3c"]);
 
-        await expect(auction.connect(signer2).claimBid()).to.be.revertedWith("Only owner and winner can claim")
-        await auction.connect(signer1).claimBid();
+        await hre.network.provider.send("hardhat_mine", ["0x3e8", "0x3c"]); // mine 1000 blocks with an interval of 1 minute
 
-        auctionIsActive1 = auction.connect(signer1).getIfAuctionIsActive();
-
-        auctionIsActive1 = await auction.connect(signer1).getIfAuctionIsActive();
-        expect(auctionIsActive1).to.be.false;
-
-
-        // Confirming the ETH are sent to the owner
-        ownerBalance1 = await owner.getBalance();
-        expect(ownerBalance1).to.be.greaterThan(ownerBalance0);
-
-        // Confirming the ERC20 are sent to the WINNER
-        winnerERC20balance = await ERC20.balanceOf(await signer1.getAddress());
-        expect(winnerERC20balance).to.be.equal(21000000);
+        await expect(auction.connect(signer3).claimBid()).to.be.revertedWith("not owning any winner bid")
     });
 
-    it('prevents to stake after the auction is over', async () => {
-        await expect(auction.connect(signer2).auctionBid({ value: '30000000000000000000' })).to.be.revertedWith("auction finished");
+    it('prevents singer5 from claimin, as not sending enough ETH', async () => {
+        await expect(auction.connect(signer5).claimBid({ value: '0' })).to.be.revertedWith("ETH not enough")
+    });
+
+    it('allows singer5 to claim', async () => {
+        await auction.connect(signer5).claimBid({ value: '10000000' });
+        signer5ERC20balance = await ERC20.balanceOf(await signer5.getAddress());
+        expect(signer5ERC20balance).to.be.equal(8);
+    });
+
+    it('prevents singer5 to claim again', async () => {
+        await expect(auction.connect(signer5).claimBid({ value: '10000000' })).to.be.revertedWith("bidder already claimed")
+    });
+
+    it('prevents signers to bid after the auction finished', async () => {
+        await expect(auction.connect(signer5).auctionBid(500, 1)).to.be.revertedWith("auction finished")
     });
 })
--- test/unit/AuctionFactory.test.js
+++ test/unit/AuctionFactory.test.js
@@ -7,8 +7,6 @@ const BN = require('bn.js');
 // Enable and inject BN dependency
 chai.use(require('chai-bn')(BN));
 
-const auctionMinBid = new ethers.BigNumber.from('100000000000000000');
-const auctionMinPrice = new ethers.BigNumber.from('1000000000000000000');
 const auctionDuration = 60 * 60; // 1 hour in seconds
 const ERC20Name = "AuctionCoin";
 const ERC20Symbol = "AC"; 
@@ -24,13 +22,13 @@ describe('AuctionFactoryUnit Test', function() {
   })
 
   it('only allows owner to raise auctions', async () => {
-    await expect(auctionFactory.connect(signer0).createANewAuction(auctionMinBid, auctionMinPrice, auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply)).to.be.revertedWith("only owner");
+    await expect(auctionFactory.connect(signer0).createANewAuction(auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply)).to.be.revertedWith("only owner");
   })
 
   it('allows owner to raise a new auction', async () => {
     const numberOfAuctionsInit = await auctionFactory.connect(owner).getNumberOfAuctions();
     expect(numberOfAuctionsInit).to.be.equal(0);
-    await auctionFactory.connect(owner).createANewAuction(auctionMinBid, auctionMinPrice, auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply);
+    await auctionFactory.connect(owner).createANewAuction(auctionDuration, ERC20Name, ERC20Symbol, ERC20Supply);
     const numberOfAuctions = await auctionFactory.connect(owner).getNumberOfAuctions();
     expect(numberOfAuctions).to.be.equal(1);
   })
